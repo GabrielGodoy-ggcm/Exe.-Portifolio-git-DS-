3ccbfdecb99a191c2387be1db702b83bf3f0cf44
GabrielGodoy-ggcm <ggcm@cin.ufpe.br>
2026-04-07 19:00:59 -0300

    feat(hello-world): adiciona programa inicial em C++

---
+++ projeto/hello-world/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+    string nome = "Gabriel";
+    cout << "Olá, " << nome << "!" << endl;
+    cout << "Bem-vindo ao portfólio Git de Desenvolvimento de Software." << endl;
+    return 0;
+}
